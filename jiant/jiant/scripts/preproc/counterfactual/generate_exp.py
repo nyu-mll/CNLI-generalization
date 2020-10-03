3ec6640e45866c6fb75a5d59650ad029c77069a8
wh629 <60375376+wh629@users.noreply.github.com>
2020-10-02 22:05:58 -0400

    fixed output for exp command path

--- jiant/jiant/scripts/preproc/counterfactual/generate_exp.py
+++ jiant/jiant/scripts/preproc/counterfactual/generate_exp.py
@@ -96,7 +96,7 @@ def main(args: RunConfiguration):
             )
         )
 
-    with open(os.path.join(args.exp_command_path,f'{args.train}-{args.val}',f'submit_exp_{task_name}_{now}.sh'), "a") as f:
+    with open(os.path.join(args.exp_command_path,f'submit_exp_{task_name}_{now}.sh'), "a") as f:
         for command in commands:
             f.write(command)
 
