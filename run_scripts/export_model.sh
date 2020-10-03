1d674a670b0d943f946705b56cf839745cfa364e
wh629 <60375376+wh629@users.noreply.github.com>
2020-10-02 20:43:51 -0400

    run scripts

---
+++ run_scripts/export_model.sh
@@ -0,0 +1,14 @@
+MODEL_TYPE=$1
+
+cd ..
+BASE_DIR=${PWD}
+source activate ./env
+export PYTHONPATH=${BASE_DIR}/jiant:$PYTHONPATH
+
+cd jiant
+
+python jiant/proj/scripts/preproc/export_model.py \
+    --model_type ${MODEL_TYPE} \
+    --output_base_path ${BASE_DIR}/models/${MODEL_TYPE}
+
+echo Downloaded ${MODEL_TYPE} to ${BASE_DIR}/models/${MODEL_TYPE}
---
+++ run_scripts/make_data_config.sh
@@ -0,0 +1,19 @@
+TRAIN=$1
+VAL=$2
+
+cd ..
+BASE_DIR=${PWD}
+source activate ./env
+export PYTHONPATH=${BASE_DIR}/jiant:$PYTHONPATH
+
+cd jiant
+
+DATA_DIR=${BASE_DIR}/data/preprocessed
+
+python jiant/scripts/preproc/counterfactual/make_data_config.py \
+    --data_base_path ${DATA_DIR}/data \
+    --output_base_path ${DATA_DIR}/ \
+    --train ${TRAIN} \
+    --val ${VAL}
+
+echo Created ${DATA_DIR}/configs/${TRAIN}-${VAL}.json
